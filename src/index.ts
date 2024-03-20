8f043085a8f5b5b12eb0de2950f5a5760718d45f
Vinicius F <artixsproductions@gmail.com>
2024-03-20 20:38:52 -0300

    refactor: movido mongoose.connect para index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import 'dotenv/config';
 import { server } from './server/Server';
+import mongoose from 'mongoose';
+
+const uri : string = process.env.MONGODB_URI as string
+
+mongoose.connect(uri)
+  .then(res => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(err => {
+    console.log("Error connecting to MongoDB");
+  })
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -1,15 +1,5 @@
 import mongoose from 'mongoose';
 
-const url : string = process.env.MONGODB_URL as string
-
-mongoose.connect(url)
-    .then(res => {
-        console.log("Connected to MongoDB");
-    })
-    .catch(err => {
-        console.log("Error connecting to MongoDB");
-    })
-
 export interface IUser {
     user: string;
     email: string;
