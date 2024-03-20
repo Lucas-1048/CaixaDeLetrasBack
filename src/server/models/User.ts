1f0a9d0037c6ce02827daeacc3d2e87891b0406b
Vinicius F <artixsproductions@gmail.com>
2024-03-20 11:10:32 -0300

    fix: colocado angle brackets no metodo model tambem

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -6,7 +6,7 @@ export interface IUser {
     password: string;
 }
 
-export const User = mongoose.model('User', new mongoose.Schema<IUser>({
+export const User = mongoose.model<IUser>('User', new mongoose.Schema<IUser>({
     user: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
