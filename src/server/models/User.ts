8651e09d3d56d46160cfb31c45c29af5d7ae9a38
Luiz Fernando Gonçalves Rocha <108113333+LuizFGRocha@users.noreply.github.com>
2024-03-22 19:11:12 +0000

    fix: "username" marcado como unique.

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -10,7 +10,7 @@ export interface IUser {
 }
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     birthDate: {type: Date, required: true},
