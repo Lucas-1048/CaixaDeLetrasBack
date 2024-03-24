622345db1678c8e6f08fc3dc32d449149d3a75a7
Lucas-1048 <lucas.almeida1048@gmail.com>
2024-03-24 00:45:24 -0300

    refactor: Diretório de rotas subdividido nas entidades Films e Users

--- src/server/routes/Users.ts
+++ src/server/routes/Users.ts
@@ -4,15 +4,12 @@ import { bodyValidation } from '../middleware/BodyValidation';
 import { VerifySignUp } from '../middleware/VerifySignUp';
 import { signUp } from '../controllers/SignUp'
 import { login } from '../controllers/Login';
-import { genres } from '../controllers/Genres';
 import { checkJwtToken } from '../middleware/JWTAuth';
 import { account } from '../controllers/Account';
 
 export const router = Router();
 
 router.post('/login', bodyValidation(loginValidation), login);
-
-router.get('/genres', genres);
 
 router.post('/signup', [ bodyValidation(VerifySignUp.signUpValidation), 
     VerifySignUp.checkDuplicateEmail, VerifySignUp.checkDuplicateUsername ], signUp)
