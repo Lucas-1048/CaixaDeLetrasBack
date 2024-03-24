709b586c51474d14b1cbf63984f068203bc836bb
Lucas-1048 <lucas.almeida1048@gmail.com>
2024-03-24 00:46:34 -0300

    style: Código do Server mais enxuto

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import { router } from './routes/Route';
+import { router } from './routes/Users';
 import cookieParser from 'cookie-parser';
 
 const server = express();
 
-server.use(cookieParser());
-
-server.use(express.json());
-
-server.use(router);
+server
+  .use(cookieParser())
+  .use(express.json())
+  .use(router);
 
 export { server };
