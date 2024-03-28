aaa76c67efc341bcc7a727e25d728284e0ed06de
Lucas-1048 <lucas.almeida1048@gmail.com>
2024-03-27 21:28:45 -0300

    style: Padronização de erros

--- src/server/controllers/Genres.ts
+++ src/server/controllers/Genres.ts
@@ -6,9 +6,7 @@ export const genres = async (_req: Request, res: Response) => {
     try {
         const genres = await Movie.distinct('genres');
         return res.status(StatusCodes.OK).json({ genres });
-    } catch (err: any) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ 
-            message: err.message 
-        });
+    } catch (err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
     }
 }
