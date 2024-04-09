2e40af2e511a33e5178c891ec685dd802d8f1b73
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-08 23:07:38 -0300

    feature: adicionado método getScore no modelo Movie

--- src/server/models/Movie.ts
+++ src/server/models/Movie.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Review } from './Review';
 
 export interface IMovie {
     title: string,
@@ -9,13 +10,28 @@ export interface IMovie {
     thumbnail?: string,
 }
 
-const MovieSchema = new mongoose.Schema({
+interface IMovieMethods {
+    getScore(): number;
+}
+
+type MovieModel = mongoose.Model<IMovie, {}, IMovieMethods>;
+
+const MovieSchema = new mongoose.Schema<IMovie, MovieModel, IMovieMethods>({
     title: {type: String, required: true},
     year: {type: Number, required: true},
     cast: {type: [String], required: true},
     genres: {type: [String], required: true},
     extract: {type: String},
     thumbnail: {type: String},
+});
+
+MovieSchema.method('getScore', function getScore() {
+    return Review.find({movie: this._id}).then((reviews) => {
+        if (reviews.length === 0) return 0;
+
+        const total = reviews.reduce((acc, review) => acc + review.rating, 0);
+        return total / reviews.length;
+    });
 });
 
 export const Movie = mongoose.model<IMovie>('Movie', MovieSchema);
