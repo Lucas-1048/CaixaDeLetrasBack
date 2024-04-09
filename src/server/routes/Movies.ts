941ce5aa35de5e5bce3ccb22b61a0288df97a15d
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-08 23:41:20 -0300

    feature: adicionadas checagem de token e de id

--- src/server/routes/Movies.ts
+++ src/server/routes/Movies.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { genres } from '../controllers/Genres';
 import { suggestions } from '../controllers/Suggestions';
+import { checkJwtToken } from '../middleware/JWTAuth';
+import { Checks } from '../middleware/Checks';
 
 const router = Router();
 
 router.get('/genres', genres);
 
-router.get('/suggestions/:id', suggestions);
+router.get('/suggestions/:idUser', [ checkJwtToken, Checks.checkParamUserId ], suggestions);
 
 export { router as moviesRouter };
