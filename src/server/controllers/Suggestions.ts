7517a504e4341cf98c8c264dcbd2318b388ba4d4
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-08 23:42:15 -0300

    refactor: usuário puxado dos locals da res

--- src/server/controllers/Suggestions.ts
+++ src/server/controllers/Suggestions.ts
@@ -1,16 +1,15 @@
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import { Movie } from '../models/Movie';
-import { User } from '../models/User';
 
 export const suggestions = async (req: Request, res: Response) => {
     try {
-        const userGenres = await User.findById(req.params.id).select('genres');
+        const userGenres = res.locals.user.genres;
 
         const suggestions = [];
 
         if (userGenres != null) {
-            for (let genre of userGenres.genres) {
+            for (let genre of userGenres) {
                 const movies = await Movie.find({ genres: genre }).limit(10).select('_id title thumbnail');
                 const moviesWithScores = await Promise.all(movies.map(async movie => {
                     const score = await movie.getScore();
