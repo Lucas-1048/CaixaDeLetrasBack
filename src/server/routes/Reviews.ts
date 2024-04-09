e2aff1265940fa95f2bfd8d584405a7765c51d44
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-09 23:22:42 +0000

    refactor: router renomeado para melhorar coesão com outros arquivos

--- src/server/routes/Reviews.ts
+++ src/server/routes/Reviews.ts
@@ -3,12 +3,14 @@ import { Checks } from '../middleware/Checks';
 import { reviewHandler } from '../controllers/Review';
 import { checkJwtToken } from '../middleware/JWTAuth';
 
-export const reviewRouter = Router();
+export const router = Router();
 
-reviewRouter.post('/review/:idUser', [checkJwtToken, Checks.checkParamUserId, Checks.checkBodyMovieId], reviewHandler.createReview);
+router.post('/review/:idUser', [checkJwtToken, Checks.checkParamUserId, Checks.checkBodyMovieId], reviewHandler.createReview);
 
-reviewRouter.delete('/review/:idUser', [checkJwtToken, Checks.checkParamUserId, Checks.checkBodyMovieId], reviewHandler.removeReview);
+router.delete('/review/:idUser', [checkJwtToken, Checks.checkParamUserId, Checks.checkBodyMovieId], reviewHandler.removeReview);
 
-reviewRouter.put('/review/:idUser', [checkJwtToken, Checks.checkParamUserId, Checks.checkBodyMovieId], reviewHandler.putReview);
+router.put('/review/:idUser', [checkJwtToken, Checks.checkParamUserId, Checks.checkBodyMovieId], reviewHandler.putReview);
 
-reviewRouter.get('/review/:reviewId', [Checks.checkParamReviewId], reviewHandler.getReview);
+router.get('/review/:reviewId', [Checks.checkParamReviewId], reviewHandler.getReview);
+
+export { router as reviewsRouter}
