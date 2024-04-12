4808543c6fea142b880b248f13fd97fd29d60fbb
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-12 19:49:18 +0000

    fix: eliminada ambiguidade da rota

--- src/server/routes/Users.ts
+++ src/server/routes/Users.ts
@@ -42,7 +42,7 @@ router.put('/favorites/:idUser/:idMovie', [ checkJwtToken, Checks.checkParamUser
 router.delete('/favorites/:idUser/:idMovie', [ checkJwtToken, Checks.checkParamUserId, Checks.checkParamMovieId ],
     accountHandler.removeFavorite);
 
-router.get('/profile/reviews', [ checkJwtToken, Checks.checkQueryUsername, bodyValidation(Checks.pageValidation) ], 
+router.get('/listReviews', [ Checks.checkQueryUsername, bodyValidation(Checks.pageValidation) ], 
     accountHandler.getReviews);
     
 router.get('/searchUser', [bodyValidation(Checks.searchUserValidation)], searchHandler.searchUser);
