e5353463aabd3d5b7b8bf267915fb5146618f582
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-12 17:20:57 +0000

    feature: adicionadas timestamps nas reviews

--- src/server/models/Review.ts
+++ src/server/models/Review.ts
@@ -6,6 +6,8 @@ export interface IReview {
     movie: mongoose.Types.ObjectId;
     review: string;
     rating: number;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 const ReviewSchema = new mongoose.Schema({
@@ -13,6 +15,6 @@ const ReviewSchema = new mongoose.Schema({
     movie: {type: ObjectId, ref:'Movie', required: true},
     review: {type: String, required: true},
     rating: {type: Number, required: true, min: 0, max: 5},
-});
+}, {timestamps: true});
 
 export const Review = mongoose.model<IReview>('Review', ReviewSchema);
