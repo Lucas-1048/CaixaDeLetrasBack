d664fa386218c1cdb2d2bd0748b0e5b39122be1b
Luiz Fernando Gonçalves Rocha <luizfgrocha04@gmail.com>
2024-04-12 17:27:40 +0000

    feature: implementada busca de reviews recentes de um filme

--- src/server/controllers/Movie.ts
+++ src/server/controllers/Movie.ts
@@ -1,6 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import { Movie } from '../models/Movie';
+import { Review } from '../models/Review';
+import { readdirSync } from 'fs';
 
 const genres = async (_req: Request, res: Response) => {
     try {
@@ -22,7 +24,34 @@ const getMovie = (req: Request, res: Response) => {
     }
 }
 
+const getReviews = async (req: Request, res: Response) => {
+    try {
+
+        const { limit = 10, page = 1 } = req.body;
+
+        const reviews = await Review.aggregate([
+            { $match: { movie: res.locals.movie._id } },
+            { $sort: { createdAt: -1 } },
+            { $skip: Number(page) * Number(limit) },
+            { $limit: Number(limit) },
+            { $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'user' } },
+            { $unwind: '$user' },
+            { $project: { _id: 1, username: '$user.username', review: 1, rating: 1, createdAt: 1 } }
+        ]);
+
+        const resPage = {
+            currentPage: Number(req.query.page),
+            totalPages: Math.ceil(await Review.find({ movie: res.locals.movie._id }).countDocuments()) / Number(limit),
+            size: Number(reviews.length),
+        };
+        return res.status(StatusCodes.OK).json({ reviews, page: resPage });
+    } catch (err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    }
+}
+
 export const movieHandler = {
+    getReviews,
     genres,
     getMovie
 };
